7c044cbde601b4dbad4c1496bb84a4219431dd36
Nathan Richardson <nathan.richardson94@gmail.com>
2019-03-25 15:57:50 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -6,7 +6,7 @@ def myMethod(integer)
   primes.each do |p|
     next unless p 
     break if p*p > 1000000
-    counter += log10
+    counter += 
     (p*p).step(1000000,p) { |m| primes[m] = nil}
   end
   primes.compact
