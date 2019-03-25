5b0a09276131ede835517590b023651000cf8954
Nathan Richardson <nathan.richardson94@gmail.com>
2019-03-25 15:57:25 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,13 +1,13 @@
 # Add  code here!
 def myMethod(integer)
-  primes = (0..max).to_a
+  primes = (0..1000000).to_a
   primes[0] = primes[1] = nil
   counter = 0 
   primes.each do |p|
     next unless p 
-    break if p*p > max
+    break if p*p > 1000000
     counter += log10
-    (p*p).step(max,p) { |m| primes[m] = nil}
+    (p*p).step(1000000,p) { |m| primes[m] = nil}
   end
   primes.compact
 end
