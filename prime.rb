5dd0a5701e670af4cadf64669fe0dfaeb2f03f5b
Nathan Richardson <nathan.richardson94@gmail.com>
2019-03-25 15:35:59 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,13 @@
 # Add  code here!
-def prime?(integer)
+def (integer)
   primes = (0..max).to_a
-  primes[0] = primes[1] = 
+  primes[0] = primes[1] = nil
+  counter = 0 
+  primes.each do |p|
+    next unless p 
+    break if p*p > max
+    counter += log10
+  
 end
 
 puts prime?(4)
